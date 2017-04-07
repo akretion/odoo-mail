8ff9e505d7f82c9ff5a9e1a03185a9a1c404f26c
MonsieurB <eric@ebii.fr>
2017-04-07 10:01:59 +0200

    [MIG] mail_filter: Migration to 10.0

---
+++ mail_filter/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Copyright <2017> Akretion
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{'name': 'mail_filter',
+ 'version': '10.0.1.0.0',
+ 'author': 'Akretion',
+ 'website': 'www.akretion.com',
+ 'license': 'AGPL-3',
+ 'category': 'Generic Modules',
+ 'depends': [
+     'mail',
+ ],
+ 'data': [
+     'views/mail_view.xml',
+ ],
+ 'installable': True,
+ 'application': False,
+}
---
+++ mail_filter/models/mail.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+# Copyright <2017> Akretion
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import models, api, fields
+import logging
+_logger = logging.getLogger(__name__)
+
+
+class MailMail(models.Model):
+    _inherit='mail.mail'
+
+    spamoo_blocked = fields.Boolean()
+
+    @api.multi
+    def send(self, auto_commit=False, raise_exception=False):
+        if self._context.get('I_really_want_to_send_this_mail'):
+            return super(MailMail, self).send(
+                auto_commit=auto_commit,
+                raise_exception=raise_exception)
+        else:
+            _logger.error("Spamoo have tried to send an email!")
+            self.write({'spamoo_blocked': True})
+        return True
+
+    @api.multi
+    def send_for_real(self):
+        return self.with_context(I_really_want_to_send_this_mail=True).send()
+
+    @api.model
+    def _get_domain_mail_to_really_send(self):
+        return []
+
+    @api.model
+    def _domain_filter_defined(self):
+        return False
+
+    @api.model
+    def process_email_queue(self, ids=None, context=None):
+        if self._domain_filter_defined():
+            domain = self._get_domain_mail_to_really_send()
+            if context is None:
+                context = {}
+            context.update({
+                'I_really_want_to_send_this_mail': True,
+                'filters': domain,
+                })
+            return super(MailMail, self).process_email_queue(
+                ids=ids, context=context)
+        return True
